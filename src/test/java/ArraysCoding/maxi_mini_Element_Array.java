356aa57dd8c501df03e95b5555b83b45f1d7c0bf
GitHub@951 <rajavenkateswarlu951@gmail>
2023-05-30 11:33:24 +0530

    Strings and Arrays Coding programs are added

---
+++ src/test/java/ArraysCoding/maxi_mini_Element_Array.java
@@ -0,0 +1,27 @@
+package ArraysCoding;
+
+public class maxi_mini_Element_Array {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub 
+		int a[] = {12,2,4,6,7,13,30,55,0};
+		int maxi = a[0];
+		int min = a[0];
+		/*
+		for(int i =1 ; i< a.length ; i++) {
+			if(a[i] > maxi) {
+				maxi = a[i] ;
+			}
+		}
+		System.out.println("Maximum form Array " + maxi);
+	*/
+		
+		for (int i = 0 ; i< a.length; i++) {
+			if(a[i] < min) {
+				min = a[i];
+			}
+		}
+		System.out.println(" Minimum value "  + min);
+	}
+
+}
